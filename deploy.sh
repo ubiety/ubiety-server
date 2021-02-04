23a9fe3ec58e14b5a51dd0d75b1c98cea378b126
Dieter Lunn <coder2000@gmail.com>
2021-02-04 15:11:23 -0600

    refactor: remove environment from deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,4 @@
 
 yaml-merge traefik.compose.yml portainer.compose.yml zammad.compose.yml > docker-compose.yml
 
-set -a
-source .env
-set +a
-
 sudo -E docker stack deploy -c docker-compose.yml ubiety
