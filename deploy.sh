c4c0a1dfefeeffdeed3eff07f0e7c6045e350582
Dieter Lunn <coder2000@gmail.com>
2021-02-04 12:25:01 -0600

    fix: make sure sudo passes environment variables

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 yaml-merge traefik.compose.yml portainer.compose.yml zammad.compose.yml > docker-compose.yml
 
 set -a && . .env && set +a
-sudo docker stack deploy -c docker-compose.yml ubiety
+sudo -E docker stack deploy -c docker-compose.yml ubiety
